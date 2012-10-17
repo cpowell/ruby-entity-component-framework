78f181b20c403f8c7ee197fd0f2498fcaceeb998
Chris Powell <cpowell@prylis.com>
2012-10-17 16:17:51 -0700

    This quashes a weird little error.

--- bin/pong_runner.rb
+++ bin/pong_runner.rb
@@ -4,11 +4,20 @@
 
 # rake rawr:jar && java -jar package/jar/pong.jar
 
-$:.push File.expand_path('../../lib/java', __FILE__)
+$:.push File.expand_path('../../lib', __FILE__)
 
 require 'java'
 require 'lwjgl.jar'
 require 'slick.jar'
+
+java_import org.newdawn.slick.BasicGame
+java_import org.newdawn.slick.GameContainer
+java_import org.newdawn.slick.Graphics
+java_import org.newdawn.slick.Image
+java_import org.newdawn.slick.Input
+java_import org.newdawn.slick.SlickException
+java_import org.newdawn.slick.AppGameContainer
+
 
 require 'lib/pong_game.rb'
 
--- lib/pong_game.rb
+++ lib/pong_game.rb
@@ -1,11 +1,3 @@
-java_import org.newdawn.slick.BasicGame
-java_import org.newdawn.slick.GameContainer
-java_import org.newdawn.slick.Graphics
-java_import org.newdawn.slick.Image
-java_import org.newdawn.slick.Input
-java_import org.newdawn.slick.SlickException
-java_import org.newdawn.slick.AppGameContainer
-
 require "lib/ball"
 require "lib/paddle"
 
