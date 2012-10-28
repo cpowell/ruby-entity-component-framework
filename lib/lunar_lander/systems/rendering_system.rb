ba46be81092db0fe340208850e118632633eda46
Chris Powell <cpowell@prylis.com>
2012-10-27 19:24:23 -0700

    Nicer fuel display.

--- lib/lunar_lander/systems/rendering_system.rb
+++ lib/lunar_lander/systems/rendering_system.rb
@@ -15,7 +15,7 @@ class RenderingSystem < System
     entities = entity_mgr.get_all_entities_with_component_of_type(Fuel)
     entities.each_with_index do |e, index|
       fuel_component   = entity_mgr.get_entity_component_of_type(e, Fuel)
-      graphics.draw_string("Fuel remaining #{fuel_component.remaining}", 8, container.height - 30 * (index+2))
+      graphics.draw_string("Fuel remaining #{sprintf "%.1f" % fuel_component.remaining}", 8, container.height - 30 * (index+2))
     end
 
     # Uncomment to visualize the bounding polygons:
