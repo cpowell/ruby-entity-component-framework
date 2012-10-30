dc800037f38e25e657c64d93bfdfa1cbe76f03de
Chris Powell <cpowell@prylis.com>
2012-10-30 15:58:37 -0700

    Added a copyright notice to all source code. It feels very non-DRY. :-(

--- lib/ruby/meta_entity.rb
+++ lib/ruby/meta_entity.rb
@@ -1,4 +1,22 @@
 ##
+# Copyright (c) 2012, Christopher Powell.
+#
+# This file is part of The Ruby Entity-Component Framework.
+#
+# The Ruby Entity-Component Framework is free software: you can redistribute it
+# and/or modify it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# The Ruby Entity-Component Framework is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with The Ruby Entity-Component Framework.  If not, see
+# <http://www.gnu.org/licenses/>.
+
 # The MetaEntity is a smarter, more powerful way of creating and handling Entities
 # in an Entity System.
 #
