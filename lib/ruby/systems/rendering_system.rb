58a37e960ce07a271d4c3e3f6f450de55cbde005
Chris Powell <cpowell@prylis.com>
2012-10-30 18:36:03 -0700

    More licensing tweaking; link to the Github page.

--- lib/ruby/systems/rendering_system.rb
+++ lib/ruby/systems/rendering_system.rb
@@ -2,20 +2,9 @@
 # Copyright 2012, Prylis Incorporated.
 #
 # This file is part of The Ruby Entity-Component Framework.
-#
-# The Ruby Entity-Component Framework is free software: you can redistribute it
-# and/or modify it under the terms of the GNU Lesser General Public License as
-# published by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# The Ruby Entity-Component Framework is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
-# General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License along
-# with The Ruby Entity-Component Framework.  If not, see
-# <http://www.gnu.org/licenses/>.
+# https://github.com/cpowell/ruby-entity-component-framework
+# You can redistribute and/or modify this software only in accordance with
+# the terms found in the "LICENSE" file included with the framework.
 
 require 'systems/system'
 require 'components/renderable'
