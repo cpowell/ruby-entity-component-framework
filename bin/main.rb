2d42c11052ba59d0a2a09667df6ca4a8b8a96908
Chris Powell <cpowell@prylis.com>
2012-12-05 22:07:15 -0800

    Correct the relative-root directory.

--- bin/main.rb
+++ bin/main.rb
@@ -14,7 +14,7 @@ $:.push File.expand_path('../../lib/', __FILE__)
 $:.push File.expand_path('../../lib/ruby/', __FILE__)
 
 # Need a different root when inside the jar, luckily $0 is "<script>" in that case
-RELATIVE_ROOT = $0['<'] ? 'ruby_ec/' : ''
+RELATIVE_ROOT = $0['<'] ? 'ecs_game/' : ''
 
 require 'java'
 require "gdx-backend-lwjgl-natives.jar"
